c5f5be812403465dc8af7fd82723a648a6091164
antonryabkin <88884655+antonryabkin@users.noreply.github.com>
2022-07-24 12:01:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const buttonAuth = document.querySelector('.button-auth')
+const buttonOut = document.querySelector('.button-out')
+const userName = document.querySelector('.user-name')
+const modalAuth = document.querySelector('.modal-auth')
+const modalError = document.querySelector('.modal-error')
+const closeAuth = document.querySelector('.close-auth')
+const loginForm = document.getElementById('logInForm')
+const inputLogin = document.getElementById('login')
+const inputPassword = document.getElementById('password')
+
+const login = (user) => {
+    buttonAuth.style.display = 'none'
+    buttonOut.style.display = 'flex'
+    userName.style.display = 'flex'
+    userName.textContent = user.login
+    modalAuth.style.display = 'none'
+}
+
+const logout = (user) => {
+    buttonAuth.style.display = 'flex'
+    buttonOut.style.display = 'none'
+    userName.style.display = 'none'
+    userName.textContent = ''
+    localStorage.removeItem('user')
+}
+
+buttonAuth.addEventListener('click', () => {
+    modalAuth.style.display = 'flex'
+})
+
+closeAuth.addEventListener('click', () => {
+    modalAuth.style.display = 'none'
+})
+
+buttonOut.addEventListener('click', () => {
+    logout()
+})
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const user = {
+        login: inputLogin.value,
+        password: inputPassword.value
+    }
+    if (inputLogin.length > 0) {
+        login(user)
+        localStorage.setItem('user', JSON.stringify(user))
+    }
+    else {
+        modalError.style.display = 'flex'
+        inputLogin.addEventListener('input', () => {
+            modalError.style.display = 'none'
+        })
+    }
+})
+
+if (localStorage.getItem('user')) {
+    login(JSON.parse(localStorage.getItem('user')));
+}
